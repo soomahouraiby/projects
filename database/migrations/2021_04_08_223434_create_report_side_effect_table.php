e6bb2d026babe8359e78e63367c302381dac88df
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2021-04-17 03:25:55 +0300

    DB

--- database/migrations/2021_04_08_223434_create_report_side_effect_table.php
+++ database/migrations/2021_04_08_223434_create_report_side_effect_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReportDetailsTable extends Migration
+class CreateReportSideEffectTable extends Migration
 {
     /**
      * Run the migrations.
