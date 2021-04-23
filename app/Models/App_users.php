d1b4786df307b22cc96c3e10c4f82845079f143d
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2021-04-24 01:18:23 +0300

    t3deel2

--- app/Models/App_users.php
+++ app/Models/App_users.php
@@ -11,7 +11,7 @@ class App_users extends Model
     protected $table="app_user";
     protected $fillable = [
         'app_user_no', 'app_user_name', 'app_user_phone', 'app_user_email',
-        'age',  'gender','report_count' ,
+        'age',  'gender','report_count' ,'adjective',
     ];
     protected $primaryKey = 'app_user_no';
     public $timestamps=false;
